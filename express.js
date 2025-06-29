9cc0beba22d8a1f0d496c0eab24767ae3193a592
MuhammadFaheemKhan2004 <faheemniazi2004@gmail.com>
2025-06-29 13:27:40 +0500

    submission form is added

--- express.js
+++ express.js
@@ -1,22 +1,31 @@
 import express from "express";
-const app = express();
 import cors from "cors";
+
+const app = express();
+const port = 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-const port = 3000;  
 const products = [
-  { img: 'http://localhost:3000/image.png', name: 'Laptop', price: 1000 }, 
+  { img: 'http://localhost:3000/image.png', name: 'Laptop', price: 1000 },
   { img: 'http://localhost:3000/phone.png', name: 'Phone', price: 500 },
   { img: 'http://localhost:3000/tablet.png', name: 'Tablet', price: 300 }
 ];
 
 app.get('/products', (req, res) => {
-    res.json(products);
+  res.json(products);
 });
 
+app.post('/products', (req, res) => {
+  const newProduct = req.body;
+  console.log("New product received:", newProduct);
+
+  products.push(newProduct);
+  res.status(201).json({ message: "Product added", product: newProduct });
+});
 
 app.listen(port, () => {
-    console.log('Server is running ');
+  console.log(`Server is running on http://localhost:${port}`);
 });
